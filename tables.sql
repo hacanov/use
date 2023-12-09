14a84cf83d1ddf3aa2c28607f4e63709e531d96d
hacanov <123230509+hacanov@users.noreply.github.com>
2023-12-09 10:09:04 +0300

    Add files via upload

--- tables.sql
+++ tables.sql
@@ -1,113 +1,117 @@
-CREATE TABLE Prin_report(
-	id INTEGER  PRIMARY KEY UNIQUE,
-	Date_report  DATE (100) NOT NULL,
-  	employee_id INTEGER (100) NOT NULL,
-  	description VARCHAR(100) NOT NULL,
-  	FOREIGN KEY (employee_id)
-  		REFERENCES Employee(id)
-  		ON DELETE CASCADE ON UPDATE NO ACTION
-)
-CREATE TABLE Employees( 
+
+CREATE TABLE Type_of_print ( 
 	id INTEGER PRIMARY KEY, 
-  	last_name VARCHAR (100) NOT NULL, 
-  	mail VARCHAR (100) NOT NULL, 
-  	phone INTEGER NOT NULL, 
-  	position INTEGER NOT NULL, 
-  	prin_report_id INTEGER, 
-  	FOREIGN KEY (prin_report_id) 
-  	REFERENCES Prin_report(id) 
+	names VARCHAR (100) NOT NULL 
+); 
+ 
+CREATE TABLE Workroom ( 
+	id INTEGER PRIMARY KEY, 
+	name INTEGER (100) NOT NULL, 
+	City INTEGER (100) NOT NULL, 
+	indexs  VARCHAR (100) NOT NULL, 
+	address VARCHAR (100) NOT NULL 
+); 
+CREATE TABLE Workshop ( 
+	id INTEGER PRIMARY KEY, 
+	name VARCHAR(100) NOT NULL, 
+	Workroom_id INTEGER, 
+	FOREIGN KEY (Workroom_id)  
+	REFERENCES Workroom(id)  
+	ON DELETE SET NULL ON UPDATE NO ACTION 
+); 
+CREATE TABLE Equipment ( 
+	id INTEGER PRIMARY KEY,  
+	name INTEGER (100) NOT NULL, 
+	Description INTEGER (100) NOT NULL 
+); 
+CREATE TABLE Equipment_Workshop ( 
+	id INTEGER PRIMARY KEY, 
+	Equipment_id INTEGER, 
+	Workshop_id INTEGER, 
+	FOREIGN key (Equipment_id) 
+	REFERENCES Equipment(id) 
+	ON DELETE SET NULL ON UPDATE NO ACTION, 
+	FOREIGN KEY (Workshop_id)  
+	REFERENCES Workshop(id)  
+	ON DELETE SET NULL ON UPDATE NO ACTION  
+); 
+	CREATE TABLE Employees ( 
+	id INTEGER PRIMARY KEY,  
+	last_name VARCHAR (100) NOT NULL,  
+	mail VARCHAR (100) NOT NULL,  
+	phone INTEGER NOT NULL,  
+	position INTEGER NOT NULL,  
+	prin_report_id INTEGER,  
+	FOREIGN KEY (prin_report_id) 
+	REFERENCES Prin_report(id) 
+	ON DELETE SET NULL ON UPDATE NO ACTION 
+); 
+ 
+CREATE TABLE Prin_report ( 
+  	id INTEGER PRIMARY KEY AUTOINCREMENT,  
+  	Date_report DATE NOT NULL, 
+  	employee_id INTEGER NOT NULL,  
+  	description VARCHAR(100) NOT NULL,  
+  	FOREIGN KEY (employee_id)  
+  	REFERENCES Employees(id)  
+  	ON DELETE CASCADE ON UPDATE NO ACTION  
+); 
+CREATE TABLE Client ( 
+  	id INTEGER PRIMARY KEY, 
+  	Name VARCHAR (100) NOT NULL,  
+  	mail VARCHAR (100) NOT NULL,  
+  	phone INTEGER (11) NOT NULL,  
+  	Last_name VARCHAR (100) NOT NULL,  
+  	address VARCHAR (100) NOT NULL, 
+  	indexs  VARCHAR (100) NOT NULL, 
+  	City INTEGER (100) NOT NULL 
+); 
+CREATE TABLE Orders ( 
+  	id INTEGER PRIMARY KEY, 
+  	Order_date DATE NOT NULL, 
+  	statuss VARCHAR (100) NOT NULL, 
+  	amount VARCHAR (100) NOT NULL, 
+  	Client_id INTEGER, 
+  	FOREIGN KEY (Client_id)  
+  	REFERENCES Client(id)  
   	ON DELETE SET NULL ON UPDATE NO ACTION 
-)
-CREATE TABLE Workroom(
-	id INTEGER  PRIMARY KEY,
-	name INTEGER (100) NOT NULL,
-  	City INTEGER (100) NOT NULL,
-  	indexs  VARCHAR (100) NOT NULL,
-  	address VARCHAR (100) NOT NULL
-)
-CREATE TABLE Workroom_Workshop( 
-	id INTEGER PRIMARY KEY, 
-  	FOREIGN KEY (Workshop) REFERENCES Workshop(id) 
-  	ON DELETE SET NULL ON UPDATE NO ACTION,
-  	FOREIGN KEY (Workroom)
-  	REFERENCES Workroom(id) 
-  	ON DELETE SET NULL ON UPDATE NO ACTION
-);
-CREATE TABLE Workshop( 
-	id INTEGER PRIMARY KEY, 
-  	name VARCHAR(100) NOT NULL,
-  	FOREIGN KEY (Workroom_id) 
-  	REFERENCES Workroom(id) 
-  	ON DELETE SET NULL ON UPDATE NO ACTION
-);
-CREATE TABLE Equipment_Worshop( 
-	UniquelID INTEGER PRIMARY KEY,
-  	FOREIGN key (Equipment_id)
-  	REFERENCES Equipment(id)
-  	ON DELETE SET NULL ON UPDATE NO ACTION,
-  	FOREIGN KEY (Workshop_id) 
-  	REFERENCES Workshop(id) 
+); 
+CREATE TABLE Product ( 
+  	id INTEGER PRIMARY KEY, 
+  	Name VARCHAR (100) NOT NULL, 
+  	Description INTEGER (100) NOT NULL, 
+  	price INTEGER (100) NOT NULL, 
+  	Type_of_print_id INTEGER, 
+  	FOREIGN KEY (Type_of_print_id)  
+  	REFERENCES Type_of_print(id)  
   	ON DELETE SET NULL ON UPDATE NO ACTION 
-)
-CREATE TABLE Equipment(
-	id INTEGER PRIMARY KEY, 
-  	name INTEGER (100) NOT NULL,
-  	Description INTEGER (100) NOT NULL
-)
-CREATE TABLE Client(
-    id INTEGER PRIMARY KEY,
-    Name VARCHAR (100) NOT NULL, 
-  	mail VARCHAR (100) NOT NULL, 
-  	phone INTEGER (11) NOT NULL, 
-  	Last_name VARCHAR (100) NOT NULL, 
-    address VARCHAR (100) NOT NULL,
-    indexs  VARCHAR (100) NOT NULL,
-    City INTEGER (100) NOT NULL
-)
-CREATE TABLE Orders(
-	id INTEGER PRIMARY KEY,
-	Order_date DATA (8) NOT NULL,
-	statuss VARCHAR (100) NOT NULL,
-	amount VARCHAR (100) NOT NULL,
-	FOREIGN KEY (Client_id) 
-  	REFERENCES Client(id) 
-  	ON DELETE SET NULL ON UPDATE NO ACTION
-)
-CREATE TABLE Type_of_print (
-	id INTEGER PRIMARY KEY,
-	names VARCHAR (100) NOT NULL,
-)
-CREATE TABLE Product(
-	id INTEGER PRIMARY KEY,
-	Name VARCHAR (100) NOT NULL,
-	Description INTEGER (100) NOT NULL,
-	price INTEGER (100) NOT NULL,
-	FOREIGN KEY (Type_of_print_id) 
-  	REFERENCES Type_of_print(id) 
-  	ON DELETE SET NULL ON UPDATE NO ACTION
-)
-CREATE TABLE Orders_Detail(
-	id INTEGER PRIMARY KEY,
-	quantity VARCHAR (100) NOT NULL,
-	FOREIGN KEY (Orders_id) 
-  	REFERENCES Orders(id) 
-  	ON DELETE SET NULL ON UPDATE NO ACTION,
-	FOREIGN KEY (Product_id) 
-  	REFERENCES Product(id) 
-  	ON DELETE SET NULL ON UPDATE NO ACTION
-)
-CREATE TABLE Supplier(
-    id INTEGER PRIMARY KEY,
-    Name VARCHAR (100) NOT NULL,   
-    address VARCHAR (100) NOT NULL,
-    City INTEGER (100) NOT NULL
-)
+); 
+CREATE TABLE Orders_Detail ( 
+  	id INTEGER PRIMARY KEY, 
+  	quantity VARCHAR (100) NOT NULL, 
+  	Orders_id INTEGER, 
+  	Product_id INTEGER, 
+  	FOREIGN KEY (Orders_id)  
+  	REFERENCES Orders(id)  
+  	ON DELETE SET NULL ON UPDATE NO ACTION, 
+  	FOREIGN KEY (Product_id)  
+  	REFERENCES Product(id)  
+  	ON DELETE SET NULL ON UPDATE NO ACTION 
+); 
+CREATE TABLE Supplier ( 
+  	id INTEGER PRIMARY KEY, 
+  	Name VARCHAR (100) NOT NULL,    
+  	address VARCHAR (100) NOT NULL, 
+  	City INTEGER (100) NOT NULL 
+); 
 CREATE TABLE Supplier_Product ( 
-	id INTEGER PRIMARY KEY,
-  	FOREIGN key (Supplier_id)
-  	REFERENCES Supplier(id)
-  	ON DELETE SET NULL ON UPDATE NO ACTION,
-  	FOREIGN KEY (Product_id) 
-  	REFERENCES Product(id) 
-  	ON DELETE SET NULL ON UPDATE NO ACTION 
-)
+  	id INTEGER PRIMARY KEY, 
+  	Supplier_id INTEGER, 
+  	Product_id INTEGER, 
+  	FOREIGN key (Supplier_id) 
+  	REFERENCES Supplier(id) 
+  	ON DELETE SET NULL ON UPDATE NO ACTION, 
+  	FOREIGN KEY (Product_id)  
+  	REFERENCES Product(id)  
+  	ON DELETE SET NULL ON UPDATE NO ACTION  
+); 
